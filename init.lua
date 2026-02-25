89a8ff8133b8c9a0b3ffdc78e5bdb0ca06a03150
aurora <qy72352089@gmail.com>
2026-02-25 23:04:31 +0800

    最小验证成功

---
+++ init.lua
@@ -0,0 +1,25 @@
+-- 基础
+vim.g.mapleader = " "
+vim.o.number = true
+
+-- lazy.nvim 安装
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    lazypath,
+  })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+  }
+})
